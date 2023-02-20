727ad72d41f19e54b97e96ee5cae41026ef8352b
haoyup <haoyup@sfu.ca>
2023-02-20 15:53:35 -0800

    Revert "minor change on command pwd" based on the pull request's comment
    This reverts commit 7bbd7087158205c009d1b219d7a647e95b4acc3d.

--- main.cpp
+++ main.cpp
@@ -243,9 +243,6 @@ int main(int argc, char** argv) {
 
     /* ....Implement fileserver different commands...... */
     vector<string> dir;
-    if (username != "Admin") {
-        dir.push_back(username);
-    }
     
     while (true){
         cout << endl;
